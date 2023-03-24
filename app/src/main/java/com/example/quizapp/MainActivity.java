54a9193d7860e423f53f976540feb9dc086f31b9
kinger83 <kinger83@outlook.com>
2023-03-24 14:16:02 +1100

    Basic layout complete. todo next -create a question class -populate a array of questions in quiz activity
    much more

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    // declare variables
+    public String userName = null;
+    public EditText nameTxt = null;
+    public Button beginQuizBtn = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    // assign view elements
+    @Override
+    protected void onStart() {
+        super.onStart();
+        nameTxt = findViewById(R.id.editTextTextPersonName);
+        beginQuizBtn = findViewById(R.id.beginBtn);
+    }
+    // function called when begin button clicked
+    public void onBeginClick(View view){
+        // check if name has been entered
+        userName = nameTxt.getText().toString();
+        if(userName.equals("")){
+            Toast.makeText(this, "Please enter a name before starting quiz", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // start intent and send name to quiz activity
+        Intent intent = new Intent(this, QuizActivity.class);
+        intent.putExtra("name", userName);
+        startActivity(intent);
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/quizapp/QuizActivity.java
@@ -2,13 +2,34 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    // Declare variables
+    public String userName = null;
+    public TextView welcomeNameMessageTxt = null;
 
+    // receive intent data
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        Intent intent = getIntent();
+        userName = intent.getStringExtra("name");
+    }
+
+    // set view elements
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Set name and welcome message
+        welcomeNameMessageTxt = findViewById(R.id.welcomeNameTxt);
+        welcomeNameMessageTxt.setText("Welcome to Kinger's Quiz " + userName + "!");
     }
 }
