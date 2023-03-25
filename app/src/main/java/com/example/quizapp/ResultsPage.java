61d1fbf3df8eebd706c0f743b57b6e4956e1ffac
kinger83 <kinger83@outlook.com>
2023-03-25 17:18:49 +1100

    Quit is not working, need to add sounds

--- app/src/main/java/com/example/quizapp/ResultsPage.java
+++ app/src/main/java/com/example/quizapp/ResultsPage.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ResultsPage extends AppCompatActivity {
     private int maxScore;
@@ -19,5 +21,34 @@ public class ResultsPage extends AppCompatActivity {
         userName = resIntent.getStringExtra("name");
         userScore = resIntent.getIntExtra("score", 0);
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        TextView name = findViewById(R.id.nameTxt);
+        TextView score = findViewById(R.id.scoreTxt);
+        name.setText("Congratulations " + userName +"!");
+        score.setText(String.valueOf(userScore) + "/" + String.valueOf(maxScore));
+
+    }
+
+    public void quit(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        finish();
+        finish();
+        finish();
+    }
+
+    public void newQuiz(View view){
+        Intent intent = new Intent(this, QuizActivity.class);
+        intent.putExtra("name", userName);
+        startActivity(intent);
+    }
+
+    public void mainMenu(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
