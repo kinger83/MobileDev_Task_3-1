7803a90204d8fd7adc8f9a425b6ddde6441a87b0
kinger83 <kinger83@outlook.com>
2023-03-25 17:21:42 +1100

    Quit is not working, need to add sounds

--- app/src/main/java/com/example/quizapp/ResultsPage.java
+++ app/src/main/java/com/example/quizapp/ResultsPage.java
@@ -36,6 +36,7 @@ public class ResultsPage extends AppCompatActivity {
     public void quit(View view){
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
         finish();
         finish();
         finish();
